a88d1ded9b4536b80fbf532f9485f0ebe866ca43
Shailesh Sharma <vinodw51822@gmail.com>
2026-05-28 21:37:21 +0530

    fixed some basic errors

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -64,10 +64,10 @@ userSchema.methods.isPasswordCorrect= async function (password) {
 userSchema.methods.generateAccessToken= function(){
     return jwt.sign(
         {
-            _id=this._id,
-            email=this.email,
-            username=this.username,
-            fullName=this.fullName
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullName:this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,{
             expiresIn:process.env.ACCESS_TOKEN_EXPIRY
@@ -77,10 +77,10 @@ userSchema.methods.generateAccessToken= function(){
 userSchema.methods.generateRefreshToken= function(){
     return jwt.sign(
         {
-            _id=this._id,
-            email=this.email,
-            username=this.username,
-            fullName=this.fullName
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullName:this.fullName
         },
         process.env.REFRESH_TOKEN_SECRET,{
             expiresIn:process.env.REFRESH_TOKEN_EXPIRY
